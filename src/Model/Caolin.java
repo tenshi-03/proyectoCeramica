d1296e411f7eb4571b81d289214f3ac97f2d3894
javierNb04 <javnavbon@alu.edu.gva.es>
2024-05-16 16:51:29 +0200

    objetos

---
+++ src/Model/Caolin.java
@@ -0,0 +1,84 @@
+package Model;
+
+public class Caolin {
+    private static Double maxFe = 2.0;
+    private static Double minFe = 0.3;
+    private static Double maxSi = 60.0;
+    private static Double minSi = 45.0;
+    private static Double maxAl = 38.0;
+    private static Double minAl = 25.0;
+    private static Double maxMg = 0.4;
+    private static Double maxNa = 0.8;
+    private static Double minNa = 0.04;
+    private static Double maxK = 2.5;
+    private static Double minK = 0.5;
+    private static Double maxTi = 0.5;
+    private static Double maxCa = 0.22;
+    private static Double minCa = 0.02;
+    private static Double maxPPC = 13.6;
+    private static Double minPPC = 8.0;
+
+    public static Double getMaxFe() {
+        return maxFe;
+    }
+
+    public static Double getMinFe() {
+        return minFe;
+    }
+
+    public static Double getMaxSi() {
+        return maxSi;
+    }
+
+    public static Double getMinSi() {
+        return minSi;
+    }
+
+    public static Double getMaxAl() {
+        return maxAl;
+    }
+
+    public static Double getMinAl() {
+        return minAl;
+    }
+
+    public static Double getMaxMg() {
+        return maxMg;
+    }
+
+    public static Double getMaxNa() {
+        return maxNa;
+    }
+
+    public static Double getMinNa() {
+        return minNa;
+    }
+
+    public static Double getMaxK() {
+        return maxK;
+    }
+
+    public static Double getMinK() {
+        return minK;
+    }
+
+    public static Double getMaxTi() {
+        return maxTi;
+    }
+
+    public static Double getMaxCa() {
+        return maxCa;
+    }
+
+    public static Double getMinCa() {
+        return minCa;
+    }
+
+    public static Double getMaxPPC() {
+        return maxPPC;
+    }
+
+    public static Double getMinPPC() {
+        return minPPC;
+    }
+}
